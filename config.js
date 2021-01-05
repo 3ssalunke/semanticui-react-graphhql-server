1a4df414b5196f162224e5c103f76332515ff094
3ssalunke <67055435+3ssalunke@users.noreply.github.com>
2021-01-05 22:29:57 +0530

    security changes

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGODB:
-    "mongodb+srv://ssal1276:wallmaria100@cluster0.ck0pe.mongodb.net/merng?retryWrites=true&w=majority",
-  SECRET_KEY: "qscgukaxfbjozdrthmfhjs1234857",
-};
